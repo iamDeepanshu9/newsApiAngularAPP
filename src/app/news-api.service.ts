8e1c2579b1f4488743c171b51c194af09d4e7a84
Deepanshu Kumar <deepanshu.kumar@remotestate.com>
2024-04-17 15:56:15 +0530

    new changes

---
+++ src/app/news-api.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient  } from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NewsApiService {
+
+  api_key = '6ff0c5e317ca46b6818c2486de542f08';
+
+
+  constructor(private http:HttpClient) { }
+
+
+  initSources(){
+	 return this.http.get('https://newsapi.org/v2/sources?language=en&apiKey='+this.api_key);
+  }
+
+
+  initArticles(){
+   return this.http.get('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey='+this.api_key);
+  }
+
+  getArticlesByID(source: String){
+   return this.http.get('https://newsapi.org/v2/top-headlines?sources='+source+'&apiKey='+this.api_key);
+  }
+
+
+
+}
